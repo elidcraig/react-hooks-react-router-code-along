326c3a308ed1998956b392a1ff5084473e5fc674
Eli Craig <elidcraig@gmail.com>
2022-05-29 20:51:48 -0700

    finished codealong

---
+++ src/components/Messages.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Messages() {
+  return (
+    <div>
+      <ul>
+        <li>Here's the first message</li>
+        <li>Here's another one!</li>
+        <li>Here's the last one... for now</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Messages
